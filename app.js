a95b7d43e0b085621b27e1e5ba24d65df9f38498
karamliceli2008-sketch <karamliceli2008@gmail.com>
2026-08-12 18:17:04 +0400

    Day 1

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Hello');
+console.log("Calil");
+let bookPrice = 10, bookCount=5 ;
+console.log("Umumi qiymet: ", bookPrice * bookCount);
+const bookName = "JavaScript";
+console.log("Kitab adi: ", bookName);
+//const deyismek olmaz;
+console.warn("xəbərdarlıq");
+console.error("xəta");
+console.log(Math.pow(2, 3)); // 2**3
+console.log(Math.sqrt(16)); // 4 (kokde)
+
+
