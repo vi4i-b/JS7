efdc0f7da756685c40eef72d6aca90c0dea3b74f
karamliceli2008-sketch <karamliceli2008@gmail.com>
2026-08-12 18:45:11 +0400

    Ranom 95-100

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-console.log("Calil");
-let bookPrice = 10, bookCount=5 ;
-console.log("Umumi qiymet: ", bookPrice * bookCount);
-const bookName = "JavaScript";
-console.log("Kitab adi: ", bookName);
-//const deyismek olmaz;
-console.warn("xəbərdarlıq");
-console.error("xəta");
-console.log(Math.pow(2, 3)); // 2**3
-console.log(Math.sqrt(16)); // 4 (kokde)
+let a=Math.floor(Math.random()*6)+95;
+console.log(a);
 
 
