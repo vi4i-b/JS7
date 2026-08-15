86b38cc7453c8caa2bd88c369af1be9e04ec1c31
karamliceli2008-sketch <karamliceli2008@gmail.com>
2026-08-15 16:17:52 +0400

    ss

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-let a=12345;
-let b=98765;
-let c=a*b;
-let d=Math.floor(c/1000000%10);
-console.log(d);
+//1.istifeciden alinan stringin uzunlugunu tapin.
+//2.istifeciden alinan stringinde "e" herfi olub olmadigini yoxlayin.
+//3.istifeciden alinan stringin "code" sozu ile basladigini yoxlayin.
+//4.istifeciden alinan stringin "code" sozu ile bitdiyini yoxlayin.
+//5.istifeciden alinan stringde butun t herflerinin yerine "####" qoyun.
+
+let userInput = prompt("Enter a string:");
+console.log (userInput.includes("e"));
+console.log (userInput.startsWitsh("code"));
+console.log (userInput.endsWith("code"));
+console.log (userInput.replaceAll("t", "####"));
+
 
