9fe7bc0547717eb30c909fb1e66bcd32422ce171
karamliceli2008-sketch <karamliceli2008@gmail.com>
2026-08-18 14:44:22 +0400

    sss

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-let a = prompt("4 reqemli eded daxil et");
-
-if (a && a.length === 4 && !isNaN(a)) {
-    let b = Number(a[0]) + Number(a[1]) + Number(a[2]) + Number(a[3]);
-    console.log("Rəqəmlərin cəmi:", b);
-} else {
-    alert("Sehifeni yenileyin ve 4 reqemli eded daxil edin");
-}
+let a = prompt("Soyadiniz ve adiniz:");
+let b=a.split(" ");
+console.log("Salam "+ b[1]+"!");
