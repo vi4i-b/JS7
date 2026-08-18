3c6be95d5da2c162934c8e81174bb7166dcf25a7
karamliceli2008-sketch <karamliceli2008@gmail.com>
2026-08-18 15:53:46 +0400

    ss

--- app.js
+++ app.js
@@ -1,29 +1,52 @@
-let a=+prompt("ayin nomresin daxil edin");
-switch (a) {
-    case a==1:
-        console.log("Yanvar");
-    case a==2:
-        console.log("Fevral");
-    case a==3:
-        console.log("Mart");
-    case a==4:
-        console.log("Aprel");
-    case a==5:
-        console.log("May");
-    case a==6:
-        console.log("Iyun");
-    case a==7:
-        console.log("iyul");
-    case a==8:
-        console.log("Avqust");
-    case a==9:
-        console.log("Sentyabr");
-    case a==10:
-        console.log("Oktyabr");
-    case a==11:
-        console.log("Noyabr");
-    case a==12:
-        console.log("Dekabr");
+let a = prompt("datani daxil edin bu formatda il.ay.gun");
+
+let c = +a.split(".")[0];
+let b = +a.split(".")[1];
+let d = +a.split(".")[2];
+
+let f = "";
+
+switch (b) {
+    case 1:
+        f = "Yanvar";
+        break;
+    case 2:
+        f = "Fevral";
+        break;
+    case 3:
+        f = "Mart";
+        break;
+    case 4:
+        f = "Aprel";
+        break;
+    case 5:
+        f = "May";
+        break;
+    case 6:
+        f = "Iyun";
+        break;
+    case 7:
+        f = "Iyul";
+        break;
+    case 8:
+        f = "Avqust";
+        break;
+    case 9:
+        f = "Sentyabr";
+        break;
+    case 10:
+        f = "Oktyabr";
+        break;
+    case 11:
+        f = "Noyabr";
+        break;
+    case 12:
+        f = "Dekabr";
+        break;
     default:
-        console.alert("Sehf");
+        console.log("Səhv ay daxil edilib");
+}
+
+if (f !== "") {
+    console.log(d + " " + f + " " + c + " il");
 }
