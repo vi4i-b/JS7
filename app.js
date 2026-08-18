0add594ac5c7d0ef6b494c41fd88dbe2983eebe9
karamliceli2008-sketch <karamliceli2008@gmail.com>
2026-08-18 15:15:40 +0400

    ss

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-let a = prompt("Soyadiniz ve adiniz:");
-let b=a.split(" ");
-console.log("Salam "+ b[1]+"!");
+let a=+prompt("ayin nomresin daxil edin");
+switch (a) {
+    case a==1:
+        console.log("Yanvar");
+    case a==2:
+        console.log("Fevral");
+    case a==3:
+        console.log("Mart");
+    case a==4:
+        console.log("Aprel");
+    case a==5:
+        console.log("May");
+    case a==6:
+        console.log("Iyun");
+    case a==7:
+        console.log("iyul");
+    case a==8:
+        console.log("Avqust");
+    case a==9:
+        console.log("Sentyabr");
+    case a==10:
+        console.log("Oktyabr");
+    case a==11:
+        console.log("Noyabr");
+    case a==12:
+        console.log("Dekabr");
+    default:
+        console.alert("Sehf");
+}
