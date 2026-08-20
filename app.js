85d7cd665336292017cced77464d1457acf36a41
karamliceli2008-sketch <karamliceli2008@gmail.com>
2026-08-20 16:54:51 +0400

    ss

--- app.js
+++ app.js
@@ -1,52 +1,5 @@
-let a = prompt("datani daxil edin bu formatda il.ay.gun");
-
-let c = +a.split(".")[0];
-let b = +a.split(".")[1];
-let d = +a.split(".")[2];
-
-let f = "";
-
-switch (b) {
-    case 1:
-        f = "Yanvar";
-        break;
-    case 2:
-        f = "Fevral";
-        break;
-    case 3:
-        f = "Mart";
-        break;
-    case 4:
-        f = "Aprel";
-        break;
-    case 5:
-        f = "May";
-        break;
-    case 6:
-        f = "Iyun";
-        break;
-    case 7:
-        f = "Iyul";
-        break;
-    case 8:
-        f = "Avqust";
-        break;
-    case 9:
-        f = "Sentyabr";
-        break;
-    case 10:
-        f = "Oktyabr";
-        break;
-    case 11:
-        f = "Noyabr";
-        break;
-    case 12:
-        f = "Dekabr";
-        break;
-    default:
-        console.log("Səhv ay daxil edilib");
+let a ="Jalil"
+function greet(name) {
+    console.log( name+ " " + "is active now");
 }
-
-if (f !== "") {
-    console.log(d + " " + f + " " + c + " il");
-}
+greet(a);
