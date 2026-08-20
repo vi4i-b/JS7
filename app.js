795d5295a09546502e9789d85cf5f6f31fddee4f
karamliceli2008-sketch <karamliceli2008@gmail.com>
2026-08-20 17:58:55 +0400

    aaaaa

--- app.js
+++ app.js
@@ -20,3 +20,22 @@ var newArr=[];
     console.log(newArr);
 }
 startWithA(b);
+
+
+
+let arrr=['2','3','4','5','6'];
+function OddNumbers(arr){
+    var count=0;
+    var count1=0
+    for(let i=0; i<arr.length; i++){
+        if(arr[i]%2!==0){
+            count++;
+        }
+        else{
+            count1++;
+        }
+    }
+    console.log("Odd numbers: " + count);
+    console.log("Even numbers: " + count1);
+}
+OddNumbers(arrr);
