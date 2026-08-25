3bbb9772b5496094d4c87c557216e948fc48a2cf
karamliceli2008-sketch <karamliceli2008@gmail.com>
2026-08-25 15:45:44 +0400

    mmmm

--- app.js
+++ app.js
@@ -12,3 +12,13 @@ while (b<100) {
     b++;
 }
 console.log(c);
+
+let t="My name";
+function capitalize(str) {
+    let words = str.split(" ");
+    for (let i=0; i<words.length; i++) {
+        words[i]=words[i][0].toUpperCase() + words[i].substr(1);
+    }
+    return words.join(" ");
+}
+console.log(capitalize(t));
